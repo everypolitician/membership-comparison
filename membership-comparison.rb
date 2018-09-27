a012faa76f8fd4da4c8f8689b31f804ec69433c6
Chris Mytton <chrismytton@gmail.com>
2018-09-27 18:21:42 +0100

    Begin implementation

--- membership-comparison.rb
+++ membership-comparison.rb
@@ -1,16 +1,30 @@
-module Membership
-  class Comparison
+module Wikidata
+  class MembershipComparison
     def initialize(existing:, suggestion:)
       @existing = existing
       @suggestion = suggestion
     end
 
     def exact_matches
-      []
+      existing.select do |_id, statement|
+        statement[:position] == suggestion[:position] &&
+          statement[:party] == suggestion[:party] &&
+          statement[:district] == suggestion[:district] &&
+          statement[:term] == suggestion[:term] &&
+          statement[:start] == suggestion[:start] &&
+          statement[:end] == suggestion[:end]
+      end.keys
     end
 
     def partial_matches
-      []
+      existing.select do |_id, statement|
+        statement[:position] == suggestion[:position] &&
+          (statement[:party] == suggestion[:party] ||
+          statement[:district] == suggestion[:district] ||
+          statement[:term] == suggestion[:term] ||
+          statement[:start] == suggestion[:start] ||
+          statement[:end] == suggestion[:end])
+      end.keys
     end
 
     def conflicts
@@ -33,20 +47,19 @@ if $PROGRAM_NAME == __FILE__
     end
   end
 
-  describe Membership::Comparison do
-
-    let(:mp) { { id: 'Q15964890' }.freeze }
-    let(:speaker) { { id: 'Q2506685' }.freeze }
-    let(:liberal) { { id: 'Q138345' }.freeze }
-    let(:conservative) { { id: 'Q488523' }.freeze }
-    let(:pontiac) { { id: 'Q3397734' }.freeze }
+  describe Wikidata::MembershipComparison do
+    let(:mp) { { id: 'Q15964890' } }
+    let(:speaker) { { id: 'Q2506685' } }
+    let(:liberal) { { id: 'Q138345' } }
+    let(:conservative) { { id: 'Q488523' } }
+    let(:pontiac) { { id: 'Q3397734' } }
     let(:term41) { { id: 'Q2816776', start: '2011-06-02', end: '2015-08-02' } }
     let(:term42) { { id: 'Q21157957', start: '2015-12-03' } }
     let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
 
     test 'no existing P39s' do
-      comparison = Membership::Comparison.new(
-        existing: [],
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {},
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
@@ -55,10 +68,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'single existing P39, previous term' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3100' => { position: mp, term: term41, party: liberal, district: pontiac }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
@@ -67,10 +80,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'single existing P39, exact match' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3101' => { position: mp, term: term42, party: liberal, district: pontiac }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal ['wds:1030-1DAA-3101']
@@ -79,11 +92,11 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'multiple existing P39s, one exact match' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3100' => { position: mp, term: term41, party: liberal, district: pontiac },
           'wds:1030-1DAA-3101' => { position: mp, term: term42, party: liberal, district: pontiac }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal ['wds:1030-1DAA-3101']
@@ -92,10 +105,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'single existing P39, partial match' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3102' => { position: mp, term: term42 }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
@@ -104,10 +117,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'single existing P39, conflict' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3102' => { position: mp, term: term42, party: conservative }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
@@ -116,10 +129,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'single existing P39, different position' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-4100' => { position: speaker, term: term41, party: liberal, district: pontiac }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
@@ -128,10 +141,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'existing dated P39, within term' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3103' => { position: mp, start: '2015-12-03', party: liberal, district: pontiac }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
@@ -140,10 +153,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'existing dated P39 between terms' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3104' => { position: mp, start: '2015-10-18', party: liberal, district: pontiac }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
@@ -152,10 +165,10 @@ if $PROGRAM_NAME == __FILE__
     end
 
     test 'existing dated P39 spanning terms' do
-      comparison = Membership::Comparison.new(
-        existing: [
+      comparison = Wikidata::MembershipComparison.new(
+        existing: {
           'wds:1030-1DAA-3105' => { position: mp, start: '2011-06-02', end: '2017-11-12', party: liberal, district: pontiac }
-        ],
+        },
         suggestion: suggestion
       )
       comparison.exact_matches.must_equal []
