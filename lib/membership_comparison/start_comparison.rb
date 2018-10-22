7105b1c1756d060e9e9ad95737a9ee9c1073dffa
Graeme Porteous <graeme@rgbp.co.uk>
2018-10-22 14:53:40 +0100

    Move conflicts to the term comparison
    Just by moving these we fix the 'existing statement, started during a
    term' specs and allows the 'Exact match when there is no actions needed'
    change to happen without specs failing.

--- lib/membership_comparison/start_comparison.rb
+++ lib/membership_comparison/start_comparison.rb
@@ -1,44 +1,19 @@
 # frozen_string_literal: true
 
 require_relative './field_comparison'
-require_relative './date_helpers'
 
 class MembershipComparison
   class StartComparison < FieldComparison
-    include DateHelpers
-
     self.field = :start
-
-    def conflict
-      if spanning_terms?
-        'spanning terms'
-      elsif previous_term_still_open?
-        'previous term still open'
-      end
-    end
 
     private
 
     def _conflict?
-      spanning_terms? || previous_term_still_open?
+      false
     end
 
     def _partial?
-      !suggestion_started_after_statement_ended? && (
-        term_started_during_statement? ||
-        (statement_open? && term_open? && term_started_after_statement?)
-      )
-    end
-
-    def spanning_terms?
-      !suggestion_started_after_statement_ended? &&
-        term_started_during_statement? &&
-        (statement_open? || term_open? || term_ended_after_statement?)
-    end
-
-    def previous_term_still_open?
-      !suggestion_started_after_statement_ended? &&
-        suggestion_started_after_statement_and_term?
+      false
     end
   end
 end
--- lib/membership_comparison/term_comparison.rb
+++ lib/membership_comparison/term_comparison.rb
@@ -1,19 +1,44 @@
 # frozen_string_literal: true
 
 require_relative './field_comparison'
+require_relative './date_helpers'
 
 class MembershipComparison
   class TermComparison < FieldComparison
+    include DateHelpers
+
     self.field = :term
+
+    def conflict
+      if spanning_terms?
+        'spanning terms'
+      elsif previous_term_still_open?
+        'previous term still open'
+      end
+    end
 
     private
 
     def _conflict?
-      false
+      spanning_terms? || previous_term_still_open?
     end
 
     def _partial?
-      false
+      !suggestion_started_after_statement_ended? && (
+        term_started_during_statement? ||
+        (statement_open? && term_open? && term_started_after_statement?)
+      )
+    end
+
+    def spanning_terms?
+      !suggestion_started_after_statement_ended? &&
+        term_started_during_statement? &&
+        (statement_open? || term_open? || term_ended_after_statement?)
+    end
+
+    def previous_term_still_open?
+      !suggestion_started_after_statement_ended? &&
+        suggestion_started_after_statement_and_term?
     end
   end
 end
