2b9d0f1664b2939ac53ec059b313e4806f6df0ae
Graeme Porteous <graeme@rgbp.co.uk>
2018-10-24 14:37:36 +0100

    Remove unused state comparison

--- lib/membership_comparison/statement_comparison.rb
+++ lib/membership_comparison/statement_comparison.rb
@@ -3,7 +3,6 @@
 require_relative './party_comparison'
 require_relative './district_comparison'
 require_relative './term_comparison'
-require_relative './start_comparison'
 
 class MembershipComparison
   class StatementComparison
@@ -40,7 +39,7 @@ class MembershipComparison
     attr_reader :statement, :suggestion, :options
 
     def comparisons
-      @comparisons ||= [party_comparison, district_comparison, term_comparison, start_comparison]
+      @comparisons ||= [party_comparison, district_comparison, term_comparison]
     end
 
     def party_comparison
@@ -53,10 +52,6 @@ class MembershipComparison
 
     def term_comparison
       TermComparison.new(statement: statement, suggestion: suggestion, options: options)
-    end
-
-    def start_comparison
-      StartComparison.new(statement: statement, suggestion: suggestion, options: options)
     end
   end
 end
