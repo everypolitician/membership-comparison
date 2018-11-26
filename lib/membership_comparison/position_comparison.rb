d1ede42edea5150d0ad3787a380c1a413cd55b80
Graeme Porteous <graeme@rgbp.co.uk>
2018-11-26 14:19:33 +0000

    Make PositionComparison a StatementComparison subclass

--- lib/membership_comparison/position_comparison.rb
+++ lib/membership_comparison/position_comparison.rb
@@ -3,39 +3,33 @@
 require_relative './statement_comparison'
 
 class MembershipComparison
-  class PositionComparison
-    def initialize(statement:, suggestion:, options:)
-      @statement = statement
-      @suggestion = suggestion
-      @options = options
-    end
+  class PositionComparison < StatementComparison
+    alias statement_state state
 
     def state
       return unless position_match? || superclass_match?
       return :ignore if bare?
       return :conflict if conflicted?
 
-      statement_comparison.state
+      super
     end
 
     def conflicts
       return [] unless position_match? || superclass_match?
       return ['position conflict'] if conflicted? && !bare?
 
-      statement_comparison.conflicts
+      super
     end
 
     def field_states
       return {} unless position_match? || superclass_match?
 
-      statement_comparison.field_states.tap do |states|
+      super.tap do |states|
         states[:position] = state if superclass_match?
       end
     end
 
     private
-
-    attr_reader :statement, :suggestion, :options
 
     def position_match?
       statement[:position] == suggestion[:position]
@@ -53,15 +47,7 @@ class MembershipComparison
       # Exact and Partial states should be considered as conflicts when we're
       # comparing superclass positions as we don't support updating these.
 
-      superclass_match? && %i[exact partial].include?(statement_comparison.state)
-    end
-
-    def statement_comparison
-      @statement_comparison ||= StatementComparison.new(
-        statement:  statement,
-        suggestion: suggestion,
-        options:    options
-      )
+      superclass_match? && %i[exact partial].include?(statement_state)
     end
   end
 end
