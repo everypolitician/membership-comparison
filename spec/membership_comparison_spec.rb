5a2a868261ee7ccad8b3db6eea89a766482ac438
Graeme Porteous <graeme@rgbp.co.uk>
2019-01-11 16:57:27 +0000

    Retitle specs contexts
    These should be in the same form/subject and hopefully will be clearer.

--- spec/membership_comparison_spec.rb
+++ spec/membership_comparison_spec.rb
@@ -17,14 +17,14 @@ describe MembershipComparison do
   let(:term41) { { id: 'Q2816776', eopt: '2011-03-26', start: '2011-06-02', end: '2015-08-02', sont: '2015-12-03' } }
   let(:term42) { { id: 'Q21157957', eopt: '2015-08-02', start: '2015-12-03' } }
 
-  context 'no existing P39s' do
+  context 'when there are no statements' do
     let(:existing) { [] }
     let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
 
     specify { expect(suggestion).to be_actionable }
   end
 
-  context 'existing base P39s' do
+  context 'when there are bare statements' do
     let(:existing) do
       [
         { position: mp },
@@ -38,7 +38,7 @@ describe MembershipComparison do
     specify { expect(existing[1]).to be_a_partial_match }
   end
 
-  context 'single existing P39, previous term' do
+  context 'when suggesting the previous term' do
     let(:existing) do
       [
         { position: mp, term: term41, party: liberal, district: pontiac },
@@ -50,7 +50,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_ignored }
   end
 
-  context 'single existing P39, previous term for different district' do
+  context 'when suggesting the next term' do
     let(:existing) do
       [
         { position: mp, term: term41, party: liberal, district: quebec },
@@ -62,7 +62,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_ignored }
   end
 
-  context 'single existing P39, following term' do
+  context 'when suggesting the next term and a different district' do
     let(:existing) do
       [
         { position: mp, term: term42, party: liberal, district: pontiac },
@@ -74,7 +74,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_ignored }
   end
 
-  context 'single existing P39, exact match' do
+  context 'when suggesting an existing statement' do
     let(:existing) do
       [
         { position: mp, term: term42, party: liberal, district: pontiac },
@@ -86,35 +86,37 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_an_exact_match }
   end
 
-  context 'multiple existing P39s, current exact match' do
-    let(:existing) do
-      [
-        { position: mp, term: term41, party: liberal, district: pontiac },
-        { position: mp, term: term42, party: liberal, district: pontiac },
-      ]
-    end
-    let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
+  context 'when there are existing statements for previous and current terms' do
+    context 'when suggesting the previous term' do
+      let(:existing) do
+        [
+          { position: mp, term: term41, party: liberal, district: pontiac },
+          { position: mp, term: term42, party: liberal, district: pontiac },
+        ]
+      end
+      let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
 
-    specify { expect(suggestion).not_to be_actionable }
-    specify { expect(existing[0]).to be_ignored }
-    specify { expect(existing[1]).to be_an_exact_match }
+      specify { expect(suggestion).not_to be_actionable }
+      specify { expect(existing[0]).to be_ignored }
+      specify { expect(existing[1]).to be_an_exact_match }
+    end
+
+    context 'when suggesting the current term' do
+      let(:existing) do
+        [
+          { position: mp, term: term41, party: liberal, district: pontiac },
+          { position: mp, term: term42, party: liberal, district: pontiac },
+        ]
+      end
+      let(:suggestion) { { position: mp, term: term41, party: liberal, district: pontiac } }
+
+      specify { expect(suggestion).not_to be_actionable }
+      specify { expect(existing[0]).to be_an_exact_match }
+      specify { expect(existing[1]).to be_ignored }
+    end
   end
 
-  context 'multiple existing P39s, historic exact match' do
-    let(:existing) do
-      [
-        { position: mp, term: term41, party: liberal, district: pontiac },
-        { position: mp, term: term42, party: liberal, district: pontiac },
-      ]
-    end
-    let(:suggestion) { { position: mp, term: term41, party: liberal, district: pontiac } }
-
-    specify { expect(suggestion).not_to be_actionable }
-    specify { expect(existing[0]).to be_an_exact_match }
-    specify { expect(existing[1]).to be_ignored }
-  end
-
-  context 'single existing P39, partial match' do
+  context 'when suggesting additional data' do
     let(:existing) do
       [
         { position: mp, term: term42 },
@@ -126,7 +128,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_a_partial_match }
   end
 
-  context 'single existing P39, party conflict' do
+  context 'when suggesting a different party' do
     let(:existing) do
       [
         { position: mp, term: term42, party: conservative },
@@ -138,7 +140,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_a_conflict.with_problem('party conflict') }
   end
 
-  context 'single existing P39, district conflict' do
+  context 'when suggesting a different district' do
     let(:existing) do
       [
         { position: mp, term: term42, party: liberal, district: quebec },
@@ -150,7 +152,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_a_conflict.with_problem('district conflict') }
   end
 
-  context 'single existing P39, different position' do
+  context 'when suggesting a different position' do
     let(:existing) do
       [
         { position: speaker, term: term41, party: liberal, district: pontiac },
@@ -162,7 +164,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_ignored }
   end
 
-  context 'single existing P39, without suggested party' do
+  context 'when not suggesting party even thought a statement has a party' do
     let(:existing) do
       [
         { position: mp, term: term42, party: conservative, district: pontiac },
@@ -171,10 +173,9 @@ describe MembershipComparison do
     let(:suggestion) { { position: mp, term: term42, party: { id: nil }, district: pontiac } }
 
     specify { expect(suggestion).not_to be_actionable }
-    specify { expect(existing[0]).to be_an_exact_match }
   end
 
-  context 'existing dated P39, within term' do
+  context 'when suggesting term started the same day as a statement' do
     let(:existing) do
       [
         { position: mp, start: '2015-12-03', party: liberal, district: pontiac },
@@ -189,7 +190,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_a_partial_match }
   end
 
-  context 'existing dated P39 between terms' do
+  context 'when suggesting term started after a statement' do
     let(:existing) do
       [
         { position: mp, start: '2015-10-18', party: liberal, district: pontiac },
@@ -204,7 +205,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_a_partial_match }
   end
 
-  context 'existing dated P39 spanning terms' do
+  context 'when suggesting term started during a statement' do
     let(:existing) do
       [
         { position: mp, start: '2011-06-02', end: '2017-11-12', party: liberal, district: pontiac },
@@ -213,13 +214,13 @@ describe MembershipComparison do
     let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
 
     # Statements: 2011-06-02 ----------------------------> 2017-11-12
-    # Term:                  -> 2015-12-03 | 2015-12-03 ->
+    # Term:                  -> 2015-08-02 | 2015-12-03 ->
 
     specify { expect(suggestion).not_to be_actionable }
     specify { expect(existing[0]).to be_a_conflict.with_problem('spanning terms') }
   end
 
-  context 'existing dated P39, later term' do
+  context 'when suggesting term ending before a statement' do
     let(:existing) do
       [
         { position: mp, start: '2015-12-03', party: liberal, district: pontiac },
@@ -234,7 +235,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_ignored }
   end
 
-  context 'existing P39s, surrounding terms' do
+  context 'when suggesting term between two other terms' do
     let(:existing) do
       [
         { position: mp, term: term40, party: liberal, district: pontiac },
@@ -251,7 +252,7 @@ describe MembershipComparison do
     specify { expect(existing[1]).to be_ignored }
   end
 
-  context 'existing P39s, surrounding dates' do
+  context 'when suggesting term between two existing statement' do
     let(:existing) do
       [
         { position: mp, start: '2008-11-18', end: '2011-03-26', party: liberal, district: pontiac },
@@ -268,7 +269,7 @@ describe MembershipComparison do
     specify { expect(existing[1]).to be_ignored }
   end
 
-  context 'existing P39s, surrounding and including terms' do
+  context 'when suggesting term between two other terms and matching term' do
     let(:existing) do
       [
         { position: mp, term: term40, party: liberal, district: pontiac },
@@ -287,7 +288,7 @@ describe MembershipComparison do
     specify { expect(existing[2]).to be_ignored }
   end
 
-  context 'existing P39s, surrounding and including dates' do
+  context 'when suggesting term between two statements and matching statement' do
     let(:existing) do
       [
         { position: mp, start: '2008-11-18', end: '2011-03-26', party: liberal, district: pontiac },
@@ -306,7 +307,7 @@ describe MembershipComparison do
     specify { expect(existing[2]).to be_ignored }
   end
 
-  context 'member returns within term' do
+  context 'when suggesting a start date after a statement within the same histrical term' do
     let(:existing) do
       [
         { position: mp, start: '2015-12-03', end: '2016-04-03', party: liberal, district: pontiac },
@@ -322,54 +323,56 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_ignored }
   end
 
-  context 'member returns within term (previous still open)' do
-    let(:existing) do
-      [
-        { position: mp, start: '2015-12-03', party: liberal, district: pontiac },
-      ]
+  context 'when there is a statement for a previous term which has not been closed' do
+    context 'when suggesting the same term' do
+      let(:existing) do
+        [
+          { position: mp, start: '2015-12-03', party: liberal, district: pontiac },
+        ]
+      end
+      let(:suggestion) { { position: mp, term: term42, start: '2017-01-03', party: liberal, district: pontiac } }
+
+      # Statements:                 2015-12-03 ------------>
+      # Term:       -> 2015-08-02 | 2015-12-03 ------------>
+      # Suggestion:                            2017-01-03 ->
+
+      specify { expect(suggestion).not_to be_actionable }
+      specify { expect(existing[0]).to be_a_conflict.with_problem('previous term still open') }
     end
-    let(:suggestion) { { position: mp, term: term42, start: '2017-01-03', party: liberal, district: pontiac } }
 
-    # Statements:                 2015-12-03 ------------>
-    # Term:       -> 2015-08-02 | 2015-12-03 ------------>
-    # Suggestion:                            2017-01-03 ->
+    context 'when suggesting the next term' do
+      let(:existing) do
+        [
+          { position: mp, start: '2015-03-10', party: liberal, district: pontiac },
+        ]
+      end
+      let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
 
-    specify { expect(suggestion).not_to be_actionable }
-    specify { expect(existing[0]).to be_a_conflict.with_problem('previous term still open') }
+      # Statements: 2015-03-10 ------------> |
+      # Term:                  -> 2015-08-02 | 2015-12-03 ->
+
+      specify { expect(suggestion).not_to be_actionable }
+      specify { expect(existing[0]).to be_a_conflict.with_problem('previous term still open') }
+    end
+
+    context 'when suggesting the current term' do
+      let(:existing) do
+        [
+          { position: mp, term: term42, start: '2016-03-03', party: liberal, district: pontiac },
+        ]
+      end
+      let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
+
+      # Statements:                            2016-03-03 ->
+      # Term:       -> 2015-08-02 | 2015-12-03 ------------>
+      # Suggestion:                 2015-12-03 ------------>
+
+      specify { expect(suggestion).not_to be_actionable }
+      specify { expect(existing[0]).to be_an_exact_match }
+    end
   end
 
-  context 'existing dated P39, started before previous term ended' do
-    let(:existing) do
-      [
-        { position: mp, start: '2015-03-10', party: liberal, district: pontiac },
-      ]
-    end
-    let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
-
-    # Statements: 2015-03-10 ------------> |
-    # Term:                  -> 2015-08-02 | 2015-12-03 ->
-
-    specify { expect(suggestion).not_to be_actionable }
-    specify { expect(existing[0]).to be_a_conflict.with_problem('previous term still open') }
-  end
-
-  context 'existing statement, started during a term' do
-    let(:existing) do
-      [
-        { position: mp, term: term42, start: '2016-03-03', party: liberal, district: pontiac },
-      ]
-    end
-    let(:suggestion) { { position: mp, term: term42, party: liberal, district: pontiac } }
-
-    # Statements:                            2016-03-03 ->
-    # Term:       -> 2015-08-02 | 2015-12-03 ------------>
-    # Suggestion:                 2015-12-03 ------------>
-
-    specify { expect(suggestion).not_to be_actionable }
-    specify { expect(existing[0]).to be_an_exact_match }
-  end
-
-  context 'when suggestied person is a disambiguation page' do
+  context 'when suggesting a disambiguation person' do
     let(:existing) do
       [
         { position: mp, term: term42, party: liberal, district: pontiac },
@@ -383,7 +386,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_a_conflict.with_problem('person is a disambiguation') }
   end
 
-  context 'when suggestied party is a disambiguation page' do
+  context 'when suggesting a disambiguation party' do
     let(:existing) do
       [
         { position: mp, term: term42, party: liberal, district: pontiac },
@@ -396,7 +399,7 @@ describe MembershipComparison do
     specify { expect(existing[0]).to be_a_conflict.with_problem('party is a disambiguation') }
   end
 
-  context 'when suggestied district is a disambiguation page' do
+  context 'when suggesting a disambiguation district' do
     let(:existing) do
       [
         { position: mp, term: term42, party: liberal, district: pontiac },
